e5d8fc40b1a8c14ea61424efb7d6706dc485c57e
Adrian Kuliński <Adrian Kuliski>
2016-03-31 20:17:18 +0200

    napisana metoda numberOfCallsInEachMinute blad strumienia przy czytaniu pliku

--- projektTin/src/projekt/tin/controller/TextFileReader.java
+++ projektTin/src/projekt/tin/controller/TextFileReader.java
@@ -1,60 +1,97 @@
 package projekt.tin.controller;
 
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
 
 public class TextFileReader {
-	public long countFileLines(String path) {
-		FileInputStream input = null;
-		BufferedReader br;
+	private String path;
+	private FileInputStream input = null;
+	private BufferedReader br;
+	private int numberOfCallsInDay = 0;
+
+	public TextFileReader(String path) {
+		this.path = path;
 		try {
 			input = new FileInputStream(path);
-			String sCurrentLine;
-			long linesCounter = 0;
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Brakuje pliku z danymi.");
+		}
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+		try {
+			input = new FileInputStream(path);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Brakuje pliku z danymi.");
+		}
+	}
+
+	public int countFileLines() {
+		int linesCounter = 0;
+		try {
 			br = new BufferedReader(new InputStreamReader(input));
-			while ((sCurrentLine = br.readLine()) != null) {
+			while (br.readLine() != null) {
 				linesCounter++;
 			}
 			br.close();
-			return linesCounter;
 		} catch (IOException e) {
-			e.printStackTrace();
-			return 0;
-		} 
-		
+			JOptionPane.showMessageDialog(null, e);
+		}
+		numberOfCallsInDay = linesCounter;
+		return linesCounter;
 	}
-	
-	//zwraca listê której indexy s¹ numerami kwadransów, wartoœci iloœci¹ wywo³añ w kwadransach
-	public void numberOfCalls() {
-		int minute;
-		int value;
-		String line;
-		String path  = "./resources/INT.TXT";
+
+	// zwraca listê której indexy s¹ numerami kwadransów, wartoœci iloœci¹
+	// wywo³añ w kwadransach
+	public List<Integer> numberOfCallsInEachMinute() {
+		int minute = 0;
+		int value = 0;
+		int tempValue = 0;
 		String sCurrentLine;
 		List<Integer> calls = new ArrayList<Integer>();
-		
-		FileInputStream input = null;
-		BufferedReader br;
-		
+		int whichStep = 0;
+		int whichQuarter = 1;
+
 		try {
-			input = new FileInputStream(path);
-			br = new BufferedReader(new InputStreamReader(input));
 			while ((sCurrentLine = br.readLine()) != null) {
-				for (String str2 : sCurrentLine.split("\\t")) {
-					System.out.println(str2);
-					
-					
+				br = new BufferedReader(new InputStreamReader(input));
+				for (String tempString : sCurrentLine.split("\\t")) {
+					if (whichStep == 0) {
+						minute = Integer.parseInt(tempString);
+					} else if (whichStep == 1) {
+						tempValue = Integer.parseInt(tempString)
+								* numberOfCallsInDay;
+						if (minute <= whichQuarter * 15) {
+							value += tempValue;
+						} else if (minute > whichQuarter * 15) {
+							calls.add(value);
+							value = 0;
+							value += tempValue;
+							whichQuarter++;
+						}
+					}
+					whichStep++;
+					if (whichStep >= 2)
+						whichStep = 0;
 				}
 			}
+			br.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e);
 		}
-		
-		return;
+		return calls;
 	}
-	
+
 }
--- projektTin/src/projekt/tin/view/MainApp.java
+++ projektTin/src/projekt/tin/view/MainApp.java
@@ -8,23 +8,24 @@ import projekt.tin.controller.TextFileReader;
 public class MainApp extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-	private long timeCount;
-	private String path = "./resources/CZAS.TXT";
+	private int timeCount;
+	private String timePath = "./resources/CZAS.TXT";
+	private String intPath = "./resources/INT.TXT";
 	
 	public MainApp(){
 		setTitle("GNR");
 		setSize(500,500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		TextFileReader fileReader = new TextFileReader();
-		timeCount = fileReader.countFileLines(path);
-		System.out.println(timeCount);
-		fileReader.numberOfCalls();
+		TextFileReader fileReader = new TextFileReader(timePath);
+		timeCount = fileReader.countFileLines();
+		fileReader.setPath(intPath);
+		fileReader.numberOfCallsInEachMinute();
 	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable(){
-			public void run(){
+			public void run(){	
 				MainApp mainApp = new MainApp();
 			}
 		});
